57524fae2ddd05b739a0058b868808969f555e2f
Ilao_Light <ethanjames.ilao@benilde.edu.ph>
2021-08-28 17:58:38 +0800

    Added Sample Project

---
+++ Sample Project/src/com/ethanilao/Main.java
@@ -0,0 +1,20 @@
+package com.ethanilao;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // Print out "Hello World"
+        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
